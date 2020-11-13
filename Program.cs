7629533d9dc48164a25e11890246b30652884bb0
DEV <dev@info.local>
2020-11-13 16:12:18 -0300

    exercicio 1 finalizado

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicios_Condicionais_C__Tarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            float Meses, Dias, Horas, Minutos;
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Digite a sua idade");
+            Console.WriteLine("------------------");
+
+            int idade = int.Parse(Console.ReadLine());
+
+            //processamento 
+
+            Meses = (idade*12);
+            Dias  = (idade*365);
+            Horas = (idade*8760);
+            Minutos = (idade*525600);
+
+
+            //saida de dados 
+
+            Console.WriteLine("Você viveu "+Meses+" Meses, "+Dias+" dias, "+Horas+" horas, "+Minutos+" e minutos.");
+
+
+        }
+    }
+}
